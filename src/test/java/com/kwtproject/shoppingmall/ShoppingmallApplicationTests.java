eabdd4312ad1adf7c8375134be60e5cfe66df7f0
kwt1326 <u1326@hotmail.com>
2021-07-19 00:46:43 +0900

    test config added

--- src/test/java/com/kwtproject/shoppingmall/ShoppingmallApplicationTests.java
+++ src/test/java/com/kwtproject/shoppingmall/ShoppingmallApplicationTests.java
@@ -3,12 +3,10 @@ package com.kwtproject.shoppingmall;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" + "classpath:/application.yml")
 
 class ShoppingmallApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
