4698d04a0f044a99682e8183718000f42a71d06e
kwt1326 <u1326@hotmail.com>
2021-08-27 17:22:04 +0900

    record change

--- nginx-ssl/init-letsencrypt.sh
+++ nginx-ssl/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.dongtaekim.space)
+domains=(apis.dongtaekim.space)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="u1326@hotmail.com" # Adding a valid address is strongly recommended
